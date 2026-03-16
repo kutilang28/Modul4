3b9d6bdb82487b40bd9548b67160b6cb22206b9c
Fidel Gandewo <fidelgandewo@gmail.com>
2026-03-17 06:52:58 +0700

    first commit

---
+++ Modul4_103022400129/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul4_103022400129
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
